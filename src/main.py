167c2a9fdeec970887ac246a663be640fd402060
Rangaraj Tirumala <rangaraj.t@gmail.com>
2017-02-19 04:01:07 +0000

    fixing shit again

--- src/main.py
+++ src/main.py
@@ -3,3 +3,15 @@ table['TEST'] = "THIS IS SOMEHTING"
 
 
 print(table.keys())
+
+
+def makeArray():
+    file = open("keywordsList.txt", "r")
+    keywordArr = []
+    for line in file:
+        emotionKeyword = line[:-1]
+        keywordArr.append(emotionKeyword)
+    print("Keyword list : ", keywordArr)
+    return
+
+makeArray()
